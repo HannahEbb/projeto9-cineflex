434349ea051a84413000c82e57ecbe39869486b6
Hannah Ebbinghaus <hannah.ebb.he@gmail.com>
2022-05-18 16:25:19 -0300

    atualizacao-de-pastas

--- src/Tela1/components/Filme/filme.js
+++ src/Tela1/components/Filme/filme.js
@@ -1,9 +1,11 @@
 
 
-export default function Filme ({posterURL}) {
+export default function Filme ({id, posterURL}) {
     return (
-        <div className="filme">
+        <div className="filme" > 
             <img width="129px" height="193px" src={posterURL} alt="HEY"/>
         </div>
     );
 }
+
+// ADD UM ONCLICK AO LADO DE "FILME" COM ARROW FUNCTION para ROTA da Tela2
