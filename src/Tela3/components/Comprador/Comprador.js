bc68def645392dc0e71f7e615fb62f42afac0f76
Hannah Ebbinghaus <hannah.ebb.he@gmail.com>
2022-05-22 17:00:18 -0300

    layout-tela3-ok

--- src/Tela3/components/Comprador/Comprador.js
+++ src/Tela3/components/Comprador/Comprador.js
@@ -1,9 +1,30 @@
 
 
 export default function Comprador () {
+
+const [digitarnome, setDigitarnome] = ("");
+const [digitarcpf, setDigitarcpf] = ("");
+
+// function reservar {
+
+// }
+
+
     return (
         <>
-        <div className="comprador">Comprador</div>
+        <div className="container-comprador">
+        <div className="dados-comprador">
+            <p>Digite o nome do comprador:</p>
+            <input placeholder="  Digite seu nome" value={digitarnome} onChange={event => setDigitarnome(event.target.value)}></input>
+        </div>
+        <div className="dados-comprador">
+            <p>CPF do comprador:</p>
+            <input placeholder="  Digite seu CPF" value={digitarcpf} onChange={e => setDigitarcpf(e.target.value)}></input>
+        </div>
+        </div>
+        <div className="container-botao">
+            <div className="botao-reservar" /*onClick={reservar}*/>Reservar assentos</div>
+        </div>
         </>
 
     );
